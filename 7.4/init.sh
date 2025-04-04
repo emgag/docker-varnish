ecf250041cf4a6788ebe89a5fc12290abbd5b20b
Matthias Blaser <git@mooch.ch>
2025-04-04 16:39:35 +0200

    Remove unsupported versions

--- 7.4/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-exec varnishd \
-    -j unix,user=vcache \
-    -F \
-    -f ${VARNISH_CONFIG} \
-    -s ${VARNISH_STORAGE} \
-    -a ${VARNISH_LISTEN} \
-    -T ${VARNISH_MANAGEMENT_LISTEN} \
-    ${VARNISH_DAEMON_OPTS}
